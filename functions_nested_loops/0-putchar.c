0bbd8cc83e19bb753945e56a7fce4af75c5a5025
batrousseau <batrousseau@hotmail.com>
2024-08-13 13:58:53 +0200

    Adding 0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+* main - Prints _putchar
+*Return: 0
+*/
+int main(void)
+{
+
+printf("_putchar\n");
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains each tasks of the project functions nested loops
