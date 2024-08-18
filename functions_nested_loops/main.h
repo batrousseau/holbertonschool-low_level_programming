9403a69e63f418b368b14d1c7b89cfaad833874b
batrousseau <batrousseau@hotmail.com>
2024-08-18 16:30:54 +0200

    Pursuing matching of one main by function, task 5

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,11 +7,11 @@
 */
 
 int _putchar (char c);
-/*void print_alphabet(void);*/
-/*void print_alphabet_x10(void);*/
-/*int _islower(int c);*/
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
 int _isalpha(int c);
-/*int print_sign(int n);*/
-/*int _abs(int);*/
-/*int print_last_digit(int n);*/
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int n);
 #endif
