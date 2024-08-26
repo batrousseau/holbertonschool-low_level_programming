8b395e304b886a2fc872b2b2602e34a799fd38d6
batrousseau <batrousseau@hotmail.com>
2024-08-26 14:45:02 +0200

    Trying a first draft of 8-24 with two loops

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ *jack_bauer - prints every minute of a day
+*/
+void jack_bauer(void)
+{
+	int m_1 = 0;
+	int m_2 = 0;
+
+	int h_1 = 0;
+	int h_2 = 0;
+
+	for (h_1 = 0; h_1 < 2 ; h_1++)
+	{
+		for (h_2 = 0; h_2 < 10 ; h_2++)
+		{
+			for (m_2 = 0; m_2 < 6 ; m_2++)
+			{
+				for (m_1 = 0; m_1 < 10 ; m_1++)
+				{
+				_putchar('0' + h_1);
+				_putchar('0' + h_2);
+				_putchar(':');
+				_putchar('0' + m_2);
+				_putchar('0' + m_1);
+				_putchar('\n');
+				}
+			}
+		}
+	}
+
+	for (h_1 = 2; h_1 < 3 ; h_1++)
+	{
+		for (h_2 = 0; h_2 < 4 ; h_2++)
+		{
+			for (m_2 = 0; m_2 < 6 ; m_2++)
+			{
+				for (m_1 = 0; m_1 < 10 ; m_1++)
+				{
+				_putchar('0' + h_1);
+				_putchar('0' + h_2);
+				_putchar(':');
+				_putchar('0' + m_2);
+				_putchar('0' + m_1);
+				_putchar('\n');
+				}
+			}
+		}
+	}
+
+}
