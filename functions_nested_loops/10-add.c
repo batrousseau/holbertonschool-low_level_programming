ba2d3b032227febef7b9e2a710719aa451ae8785
batrousseau <batrousseau@hotmail.com>
2024-09-20 15:11:06 +0200

    Suprisingly adding 10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* add - Add two integers and return the result
+* @a: an integer
+* @b: integer are two integers that are being added
+* Return: c is the result of the function
+*/
+int add(int a, int b)
+{
+
+	int c = a + b;
+
+	return (c);
+}
