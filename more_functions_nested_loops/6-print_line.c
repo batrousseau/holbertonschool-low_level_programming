72e24f9a927682f55158745451ae8e0f1d163358
batrousseau <batrousseau@hotmail.com>
2024-09-24 15:22:19 +0200

    Pushing task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* print_line - Prints straight lines in the terminal
+* @n: an integer
+* Return:void
+*/
+
+
+void print_line(int n)
+{
+
+	int i = 0;
+
+
+	for (i = 0; i < n; i++)
+	{
+	_putchar(95);
+	}
+_putchar('\n');
+}
