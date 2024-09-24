1003e92619dcb57c039d529fec3232a0c8beade9
batrousseau <batrousseau@hotmail.com>
2024-09-24 08:10:54 +0200

    Adding task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* mul - Multiplies two integers
+* @a: the first integer in the mul
+* @b: second integer to mul
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
