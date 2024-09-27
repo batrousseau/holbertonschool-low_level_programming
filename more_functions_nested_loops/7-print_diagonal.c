3832f16ea470d339ccc53c48e8c85b9766f3bf36
batrousseau <batrousseau@hotmail.com>
2024-09-27 20:32:47 +0200

    Pushing new task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* print_diagonal - draws a diagonal line
+* @n: an integer
+* Return:void
+*/
+
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n ; i++)
+		{
+		int result = 0;
+			for (result = 1; result < i ; result++)
+			{
+			_putchar(32);
+			}
+			if(j == i - 1)
+			{
+			_putchar(92);
+			_putchar('\n');
+			j++;
+			}
+		}
+	}
+}
