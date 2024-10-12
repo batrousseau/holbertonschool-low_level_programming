23d617cca2ef3bd560cbaa85bb021b41ffaa6d1c
batrousseau <batrousseau@hotmail.com>
2024-10-12 18:22:14 +0200

    Adding task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* print_triangle - draws a triangle line
+* @size: size of integer
+* Return:void
+*/
+
+
+void print_triangle(int size)
+{
+
+int i = 0;
+int j = 0;
+int k = 0;
+
+if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		for (i = size ; i != 0; i--)
+		{
+			for (j = 0; j < i; j++)
+			{
+			_putchar(32);
+			}
+			for (k = size; k > i - 1; k --)
+			{
+			_putchar(35);
+			}
+		_putchar('\n');
+		}
+	}
+}
