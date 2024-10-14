fa1acc96f3a1ce2c3df6fc88aea776b832e99e18
batrousseau <batrousseau@hotmail.com>
2024-10-14 20:01:06 +0200

    Fixing task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* _strlen - return the length of
+* a string
+* @s : a pointer to a string
+* Return:an integer
+*/
+
+
+int _strlen(char *s)
+{
+
+int i = 0;
+
+for (i = 0; *(s + i) != 0; i++)
+{
+}
+
+return (i);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,7 +5,8 @@
  * main-h : définit les fonctions utilisées, sans les déclarer
  *
 */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int (int *a, int *b);
+int _strlen(char *s);
 #endif
