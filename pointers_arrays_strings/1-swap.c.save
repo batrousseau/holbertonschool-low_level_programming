29f8d38a26bf4c5109c7b93d600caa25ec805a9e
batrousseau <batrousseau@hotmail.com>
2024-10-19 10:49:04 +0200

    Adding task 5

---
+++ pointers_arrays_strings/1-swap.c.save
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* swap_int - swaps the value of
+* two integers
+* @a: pointers to int a
+* @b: pointers to int b
+* Return:void
+*/
+
+
+
+void swap_int(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,26 +2,44 @@
 /* more headers goes there */
 
 /**
+*swap_char - swaps the value of
+* two characters
+* @a : pointers to char a
+* @b : pointers to char b
+* Return : void
+*/
+
+void swap_char (char *a, char *b)
+
+{
+char temp = *a;
+*a = *b;
+*b = temp;
+}
+
+/**
 * rev_string - reverse a string
 * @s : a pointer to a string
 * Return:nothing
 */
 
-
 void rev_string(char *s)
 {
-
 int i = 0;
-int j = 0;
-int partial_result = 0;
+int strlen = 0;
+int j = strlen / 2;
+int k = 0;
 
-	for (i = 0; *(s+i) != 0; i++)
-	{
-	partial_result = i;
-		for (j = i; j != ; j--)
-		{
-		
-		}
-	}
+/** Getting string lenght **/
+for (i = 0; *(s + i) != 0; i++)
+{
+strlen = i;
+}
+
+/** Swapping characters **/
+for (k = 0; k <= j; k++)
+{
+swap_char (s, (s + strlen));
+}
 
 }
