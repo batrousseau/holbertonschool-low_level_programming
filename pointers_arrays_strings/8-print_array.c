23257cc695d17f04d6c748586b1873469d964ac5
batrousseau <batrousseau@hotmail.com>
2024-10-19 21:11:33 +0200

    Adding task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+* print_array - print an array of
+* integers
+* @a : first element of array
+* @n : number of elements to print
+* Return : void
+*/
+
+void print_array(int *a, int n)
+{
+int i = 0;
+
+for (i = 0; i <= n - 1 ; i++)
+{
+	if (i == n - 1)
+	{
+	printf(" %d", *(a + i));
+	}
+	else
+	{
+	printf("%d, ", *(a + i));
+	}
+}
+printf("\n");
+}
