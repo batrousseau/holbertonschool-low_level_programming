4337f3c87246fb9544f2ab6f1dfdb703bd77ceb9
batrousseau <batrousseau@hotmail.com>
2024-10-19 20:10:44 +0200

    Pushing task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* puts2 - prints one every
+* two characters
+* @str : a string
+* Return : void
+*/
+
+void puts2(char *str)
+{
+int i = 0;
+
+for (i = 0; *(str +i) != 0; i++)
+{
+	if (i % 2 == 0)
+	{
+	_putchar(*(str + i));
+	}
+	else
+	{
+	}
+}
+_putchar('\n');
+}
