2c4248f367dece8a6c330ecf25ec0d01eb20f46f
batrousseau <batrousseau@hotmail.com>
2024-10-26 12:21:10 +0200

    Fixing betty code

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,17 @@
 #include "main.h"
 /**
-* _atoi - convert a string to
-* an integer
-* @s: a string
-* Return: an int
+* sign - determine the sign of
+* the number in a string
+* @s : a string
+* Return: 0
 */
-int _atoi(char *s)
+
+int sign(char *s)
 {
-int i = 0;
-int j = 0;
 int minus = 0;
 int plus = 0;
-int result = 0;
+int sign = 0;
 
-/* Getting the number */
-for (j = 0; *(s + j) != '\0'; j++)
-{
-	if (*(s + j) >=  48 && *(s + j) <= 57)
-	{
-	result = result * 10 + (*(s + j) - 48);
-		if (*(s + j + 1) < 48 || *(s + j + 1) > 57)
-		{
-		break;
-		}
-	}
-}
-
-/* Determining the sign of the number */
 for (i = 0; *(s + i) <= 48 || *(s + i) >= 57 ; i++)
 {
 	if (*(s + i) == 45)
@@ -40,12 +25,43 @@ for (i = 0; *(s + i) <= 48 || *(s + i) >= 57 ; i++)
 }
 if (minus > plus)
 {
-result = -result;
+sign = -signt;
 }
 else if (minus == plus)
 {
-result = result;
+sign = sign;
 }
+
+return (0);
+}
+
+/**
+* _atoi - convert a string to
+* an integer
+* @s: a string
+* Return: an int
+*/
+
+int _atoi(char *s)
+{
+int i = 0;
+int j = 0;
+
+int result = 0;
+
+for (j = 0; *(s + j) != '\0'; j++)
+{
+	if (*(s + j) >=  48 && *(s + j) <= 57)
+	{
+	result = result * 10 + (*(s + j) - 48);
+		if (*(s + j + 1) < 48 || *(s + j + 1) > 57)
+		{
+		break;
+		}
+	}
+}
+
+sign(result);
 
 return (result);
 }
