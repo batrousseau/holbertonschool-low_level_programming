cb6065d89afbf24ab25fe08800c867f914b480d9
batrousseau <batrousseau@hotmail.com>
2024-10-26 14:17:14 +0200

    Fixing betty and other main possibilities

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,47 @@
 #include "main.h"
 /**
-* sign - determine the sign of
-* the number in a string
+* get_number - get the absolute
+* value of a int in a string
 * @s : a string
-* Return: 0
+* Return: the number
 */
 
-int sign(char *s)
+int get_number(char *s)
+{
+int j = 0;
+
+int avalue = 0;
+
+for (j = 0; *(s + j) != '\0'; j++)
+{
+	if (*(s + j) >=  48 && *(s + j) <= 57)
+	{
+	avalue = avalue * 10 + (*(s + j) - 48);
+		if ((*(s + j + 1) < 48 || *(s + j + 1) > 57) || *(s + j + 1) == '\0')
+		{
+		break;
+		}
+	}
+}
+
+return (avalue);
+}
+
+/**
+* _atoi - convert a string to
+* an integer
+* @s: a string
+* Return: an int
+*/
+
+int _atoi(char *s)
 {
 int minus = 0;
 int plus = 0;
 int sign = 0;
+int i = 0;
+
+sign = get_number(s);
 
 for (i = 0; *(s + i) <= 48 || *(s + i) >= 57 ; i++)
 {
@@ -25,43 +56,12 @@ for (i = 0; *(s + i) <= 48 || *(s + i) >= 57 ; i++)
 }
 if (minus > plus)
 {
-sign = -signt;
+sign = -sign;
 }
 else if (minus == plus)
 {
 sign = sign;
 }
 
-return (0);
-}
-
-/**
-* _atoi - convert a string to
-* an integer
-* @s: a string
-* Return: an int
-*/
-
-int _atoi(char *s)
-{
-int i = 0;
-int j = 0;
-
-int result = 0;
-
-for (j = 0; *(s + j) != '\0'; j++)
-{
-	if (*(s + j) >=  48 && *(s + j) <= 57)
-	{
-	result = result * 10 + (*(s + j) - 48);
-		if (*(s + j + 1) < 48 || *(s + j + 1) > 57)
-		{
-		break;
-		}
-	}
-}
-
-sign(result);
-
-return (result);
+return (sign);
 }
