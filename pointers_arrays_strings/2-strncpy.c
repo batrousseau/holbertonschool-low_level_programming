ee30285c7a0096d3c5eb4a1d58f2b88cd38e34cc
batrousseau <batrousseau@hotmail.com>
2024-11-25 19:24:28 +0100

    Pushing new version of 2-strncopy with proper cleaning memory of dest

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,56 @@
 #include "main.h"
+/**
+* getting_src_lenght - get src lenght
+* @src : what you have
+* Return: src_lenght as an int
+*/
+
+int getting_src_lenght(char *src)
+{
+int i = 0;
+int src_lenght = 0;
+
+for (i = 0; *(src + i) != '\0'; i++)
+{
+}
+src_lenght = i;
+return (src_lenght);
+}
+
+/**
+* getting_dest_lenght - get dest lenght
+* @dest : what you want to copy
+* Return: dest_lenght as an int
+*/
+
+int getting_dest_lenght(char *dest)
+{
+int k = 0;
+int dest_lenght = 0;
+
+for (k = 0; *(dest + k) != '\0'; k++)
+{
+}
+dest_lenght = k;
+return (dest_lenght);
+}
 
 /**
 * _strncpy - copy one string to another
-* @dest: where you wanna go
-* @src: where you at
+* @dest: what you have
+* @src: what you wan't to copy
 * @n : an int
 * Return: pointer to dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
 int j = 0;
-int src_lenght = 0;
 
-/* Getting src lenght */
-for (i = 0; *(src + i) != '\0'; i++)
-{
-}
-src_lenght = i;
+int src_lenght = getting_src_lenght(src);
+int dest_lenght = getting_dest_lenght(dest);
 
-/* Copying strings and preventing overflow */
+/* If I don't copy the entire src, I don't care about null character*/
 
 if (src_lenght >= n)
 {
@@ -31,16 +61,16 @@ if (src_lenght >= n)
 	}
 }
 
+/* If I must copy the entire src, then I must clear the memory of dest*/
 else
 {
-	for (j = 0; *(src + j) != '\0'; j++)
+	for (j = 0 ; *(src + j) != '\0'; j++)
 	{
 	*(dest + j) = *(src + j);
 	}
-
-	if (*(src + j) == '\0')
+	for (j = src_lenght ; j <= dest_lenght; j++)
 	{
-	*(dest + j) = *(src + j);
+	*(dest + j) = '\0';
 	}
 }
 
