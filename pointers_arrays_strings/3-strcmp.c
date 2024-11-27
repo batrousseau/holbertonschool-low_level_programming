4ad37e0b12debf07c00c3f8858e08de01168317f
batrousseau <batrousseau@hotmail.com>
2024-11-27 19:01:38 +0100

    Pushing 3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,41 +11,37 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int s1_lenght = 0;
-int s2_lenght = 0;
+
 int i = 0;
 int final_result = 0;
+int s1_value = 0;
+int s2_value = 0;
 
-for (i = 0; *(s1 + i) != '\0'; i++)
+for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
 {
 }
-s1_lenght = i;
-for (i = 0; *(s2 + i) != '\0'; i++)
+s1_value = *(s1 + i);
+s2_value = *(s2 + i);
+
+if (s1_value == 0)
 {
+s1_value = 48;
 }
-s2_lenght = i;
+else
+{
+s2_value = 48;
+}
 
-
-if (s1_lenght == s2_lenght)
+if (*(s1 + i) == *(s2 + i))
 {
 final_result = 0;
 }
 
-if (s1_lenght < s2_lenght)
+else
 {
-	for (i = 0; *(s1 + i) != '\0'; i++)
-	{
-	}
-final_result = -*(s2 + i);
+final_result = s1_value - s2_value;
 }
 
-if (s1_lenght > s2_lenght)
-{
-	for (i = 0; *(s2 + i) != '\0'; i++)
-	{
-	}
-final_result = *(s1 + i);
-}
 
 return (final_result);
 }
