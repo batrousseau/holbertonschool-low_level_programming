b4a3c752c9a2c404f689b6b2400c0f02be876adc
batrousseau <batrousseau@hotmail.com>
2024-11-27 19:22:48 +0100

    Testing new 3-strcmp.c with proper understanding of it main

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -17,30 +17,19 @@ int final_result = 0;
 int s1_value = 0;
 int s2_value = 0;
 
-for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
+for (i = 0; *(s1 + i) == *(s2 + i); i++)
 {
+	if (*(s1 +i) == '\0' && *(s2 + i) == '\0')
+	{
+	final_result = 0;
+	return (final_result);
+	}
 }
-s1_value = *(s1 + i);
-s2_value = *(s2 + i);
+s1_value = *(s1 +i);
+s2_value = *(s2 +i);
 
-if (s1_value == 0)
-{
-s1_value = 48;
-}
-else
-{
-s2_value = 48;
-}
-
-if (*(s1 + i) == *(s2 + i))
-{
-final_result = 0;
-}
-
-else
-{
 final_result = s1_value - s2_value;
-}
+
 
 
 return (final_result);
