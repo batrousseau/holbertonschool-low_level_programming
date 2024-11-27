bf576b89d5e98eaabd5fe8eaef3956739ec19f60
batrousseau <batrousseau@hotmail.com>
2024-11-27 19:23:49 +0100

    Fixing betty coding style

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -19,18 +19,16 @@ int s2_value = 0;
 
 for (i = 0; *(s1 + i) == *(s2 + i); i++)
 {
-	if (*(s1 +i) == '\0' && *(s2 + i) == '\0')
+	if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
 	{
 	final_result = 0;
 	return (final_result);
 	}
 }
-s1_value = *(s1 +i);
-s2_value = *(s2 +i);
+s1_value = *(s1 + i);
+s2_value = *(s2 + i);
 
 final_result = s1_value - s2_value;
-
-
 
 return (final_result);
 }
