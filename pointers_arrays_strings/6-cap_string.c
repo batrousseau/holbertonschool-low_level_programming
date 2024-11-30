421f92a7784a45a3019605e641daacddfb4d3f3a
batrousseau <batrousseau@hotmail.com>
2024-11-30 16:47:35 +0100

    First version of 6-cap_string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,81 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+* cap1 - Do part of the cap_string job
+* @s: a string
+* Return: a string
+*/
+char *cap1(char *s)
+{
+int i = 0;
+
+for (i = 0; *(s + i) != '\0'; i++)
+{
+	if (*(s + i) == 9 || *(s + i) == 10 || *(s + i) == 32)
+	{
+		if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+		{
+		*(s + i + 1) = *(s + i + 1) - 32;
+		}
+	}
+
+	if (*(s + i) == 33 || *(s + i) == 34 || *(s + i) == 40)
+	{
+		if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+		{
+		*(s + i + 1) = *(s + i + 1) - 32;
+		}
+	}
+}
+
+return (s);
+
+}
+/**
+* cap2 - Do part of the cap_string job
+* @s: a string
+* Return: a string
+*/
+char *cap2(char *s)
+{
+
+int i = 0;
+for (i = 0; *(s + i) != '\0'; i++)
+{
+	if (*(s + i) == 41 || *(s + i) == 44 || *(s + i) == 46)
+	{
+		if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+		{
+		*(s + i + 1) = *(s + i + 1) - 32;
+		}
+	}
+
+	if (*(s + i) == 59 || *(s + i) == 63 || *(s + i) == 123)
+	{
+		if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+		{
+		*(s + i + 1) = *(s + i + 1) - 32;
+		}
+	}
+}
+
+return (s);
+}
+
+
+
+/**
+* cap_string - Uppercase every word
+* @s : a string
+* Return: a string
+*/
+
+char *cap_string(char *s)
+{
+
+char *partial = cap1(s);
+char *final = cap2(partial);
+
+return (final);
+}
