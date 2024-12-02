c3055e05711cbd5e26a7c6cc8117adb894cdaaa1
batrousseau <batrousseau@hotmail.com>
2024-12-02 19:37:34 +0100

    Adding first version of memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _memset - Fills memory with a constant byte
+* @s: a pointer towards the memory to fill
+* @b: the constant byte
+* @n: the number of byte to fill
+* Return: pointer to the s memory area
+*/
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+
+for (i = 0; i <= n; i++)
+{
+*(s + i) = b;
+}
+
+return (s);
+
+}
