08c2b1c0b7f75160a177c778a89a4dadc85556c3
batrousseau <batrousseau@hotmail.com>
2024-12-10 19:10:14 +0100

    Adding first version of memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _memcpy - copies memory area
+* @dest: the string to receive new memory
+* @src : the original string
+* @n: the unsigned int giving the lenght
+* of bytes to copy
+* Return: a pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+
+for (i = 0; i <= n; i++)
+{
+*(dest + i) = *(src + i);
+}
+
+return (dest);
+}
