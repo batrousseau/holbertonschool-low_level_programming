7c9788b692ca447cab6203504be4e1b03021f7d1
batrousseau <batrousseau@hotmail.com>
2024-12-11 08:50:51 +0100

    Pushing first try of strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character
+* in a string
+* @s: the string to look at
+* @c: character to find
+* Return: pointer to the first occurence of the
+* character, return NULL if not find
+*/
+
+
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+char *p;
+
+for (i = 0; *(s + i) != '\0'; i++)
+{
+	if (*(s + i) == c)
+	{
+	p = (s + i);
+	return (p);
+	}
+}
+
+p = "NULL";
+return (p);
+
+}
