c6efd6c9c2b2734bae977fae41d9e7e050052fa6
batrousseau <batrousseau@hotmail.com>
2024-12-14 10:03:07 +0100

    Fixing conditions for '\0' character

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -17,6 +17,7 @@ char *_strchr(char *s, char c)
 int i = 0;
 char *p = 0;
 
+
 for (i = 0; *(s + i) != '\0'; i++)
 {
 	if (*(s + i) == c)
@@ -25,19 +26,17 @@ for (i = 0; *(s + i) != '\0'; i++)
 	return (p);
 	}
 
-	else if (*(s + i) == '\0')
-	{
-	p = (s + i);
-	return (p);
-	}
-
-	else
-	{
-	p = NULL;
-	return (p);
-	}
-
 }
 
+if (c == '\0')
+{
+p = '\0';
 return (p);
+}
+
+else
+{
+p = NULL;
+return (p);
+}
 }
