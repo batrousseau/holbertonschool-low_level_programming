caa709df680cc87f0e4197e74acb08f3436ffefa
batrousseau <batrousseau@hotmail.com>
2024-12-25 00:39:42 +0100

    First try of pow

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _pow_recursion - compute one number
+* powers another
+* @x: the number to raise
+* @y: the power
+* Return: the operation
+*/
+
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+
+if (y == 0)
+{
+return (1);
+}
+
+if (y == 1)
+{
+return (x);
+}
+else
+{
+return ( x * _pow_recursion(x, y - 1));
+}
+
+}
