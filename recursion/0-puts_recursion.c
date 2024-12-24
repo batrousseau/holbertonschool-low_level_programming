735af6ed0aff9a05a9204e9a21e0947a780aeff9
batrousseau <batrousseau@hotmail.com>
2024-12-24 22:03:48 +0100

    Pushing first try of puts_recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string
+* using recursion principles
+* @s: a string
+* Return: Nothing
+*/
+
+
+void _puts_recursion(char *s)
+{
+int i = 0;
+
+if (*(s + i) == '\0')
+{
+_putchar(*(s + i));
+_putchar('\n');
+}
+else
+{
+_putchar(*(s + i));
+i++;
+_puts_recursion((s + i));
+}
+
+}
