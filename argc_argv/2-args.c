70c5843ef51ea71a3527623f367ba295cb71c672
batrousseau <batrousseau@hotmail.com>
2024-12-28 00:19:59 +0100

    First push of 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/*
+* main - prints all arguments
+* it receives
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0
+*/
+
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+int j = 0;
+
+for (i = 0; i < argc; i++)
+{
+	for (j = 0; *(argv[i] + j) != '\0'; j++)
+	{
+	_putchar(*(argv[i] + j));
+	}
+_putchar(10);
+}
+
+return (0);
+
+}
