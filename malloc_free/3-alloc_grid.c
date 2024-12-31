2dc3c1a72a23701ce44db314b8bf0a0f939f0a23
batrousseau <batrousseau@hotmail.com>
2024-12-31 13:45:12 +0100

    Functionning program with memory leak

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* alloc_grid - create a 2 dimension array
+* of integers
+* @width: number of spots in one raw
+* @height: number of sports in one column
+* Return: pointer to the array
+*/
+
+
+int **alloc_grid(int width, int height)
+{
+int i = 0;
+int j = 0;
+int **a = malloc((width * 4));
+
+if (a == NULL)
+{
+return (NULL);
+}
+
+for (i = 0; i < width; i++)
+{
+a[i] = malloc(height * 4);
+	if (a[i] == NULL)
+	{
+	return (NULL);
+	}
+}
+
+for (i = 0; i < height; i++)
+{
+	for (j = 0; j < width; j++)
+	{
+	a[i][j] = 0;
+	}
+}
+
+return (a);
+
+}
