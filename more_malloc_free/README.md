d6c923525201bbb387a2b26e0053850415840b0d
batrousseau <batrousseau@hotmail.com>
2025-01-05 16:26:05 +0100

    First push

---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This readme sums up the task I did to go deeper on dynamic memory allocation
---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* main-h : définit les fonctions utilisées, sans
+* les déclarer
+*
+*/
+
+int _putchar (char c);
+void *malloc_checked(unsigned int b);
+
+#endif
