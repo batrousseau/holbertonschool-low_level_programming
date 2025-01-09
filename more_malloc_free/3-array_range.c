853d0c7de893c0f4b13aed3f1057ba5ccfd8a986
batrousseau <batrousseau@hotmail.com>
2025-01-09 18:30:06 +0100

    First push of array range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* array_range - creates an array of integer
+* @min: the minimum value
+* @max: the maximum value
+* Return: pointer to array
+*/
+
+int *array_range(int min, int max)
+{
+int i = 0;
+int lenght = 0;
+int *p = NULL;
+
+if (min > max)
+{
+return (NULL);
+}
+
+/* Getting array lenght */
+
+for (i = min; i <= max; i++)
+{
+}
+lenght = i;
+
+p = malloc(sizeof(int) * lenght);
+if (p == NULL)
+{
+return (NULL);
+}
+
+for (i = min; i <= max; i++)
+{
+*(p + i) = i;
+}
+
+return (p);
+}
