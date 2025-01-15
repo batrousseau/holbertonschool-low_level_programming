65c7a04f729a75e9497a3219005ba961593932c6
batrousseau <batrousseau@hotmail.com>
2025-01-15 19:36:12 +0100

    First push of init dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * struct dog - a dog
@@ -16,5 +16,7 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
