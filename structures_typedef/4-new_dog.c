3cf49bace47dc696bf3197fb3348c57537d9dc89
batrousseau <batrousseau@hotmail.com>
2025-01-16 09:05:36 +0100

    Pushing new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+* new_dog - create a new dog
+* @name: his name
+* @age: his age
+* @owner: his owner
+* Return: pointer to structure
+*/
+
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+dog_t new_dog;
+dog_t *d = &new_dog;
+float *p = &d->age;
+
+if (d == NULL)
+{
+return (NULL);
+}
+
+if (name == NULL || owner == NULL || p == NULL)
+{
+return (NULL);
+}
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+
+return (d);
+
+}
