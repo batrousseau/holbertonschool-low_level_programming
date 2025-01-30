e7be68572f69829253336fe989e249255273619d
batrousseau <batrousseau@hotmail.com>
2025-01-30 17:57:05 +0100

    Push of task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print numbers, followed by a new line
+ * @separator: string beeing print beetween numbers
+ * @n: number of integers passed to the function
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+va_list args;
+int number = 0;
+
+if (separator == NULL)
+{
+va_start(args, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		number = va_arg(args, int);
+		printf("%d", number);
+	}
+	number = va_arg(args, int);
+	printf("%d\n", number);
+}
+else
+{
+va_start(args, n);
+	for (i = 0; i < n - 1; i++)
+	{
+		number = va_arg(args, int);
+		printf("%d%s", number, separator);
+	}
+	 number = va_arg(args, int);
+	printf("%d\n", number);
+}
+
+}
