143350c97f5ba7540f899c576daf02a66495fff0
batrousseau <batrousseau@hotmail.com>
2025-06-23 18:37:42 +0200

    Cleaning file to betty compliance

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,9 +6,15 @@ void print_numbers(const char *separator, const unsigned int, ...);
 void print_strings(const char *separator, const unsigned int, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct type - link a char and a function pointer
+ * @x: a char
+ * @fp: a function pointer taking a variadic list as parameter
+ */
+
 typedef  struct type
 {
-    char x;
-    void (*fp) (va_list arg);
+	char x;
+	void (*fp)(va_list arg);
 } type_a;
 #endif
