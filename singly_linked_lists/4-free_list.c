b9422d43376ff208d5e70c1ecd49d7e7054907b1
batrousseau <batrousseau@hotmail.com>
2025-06-24 19:12:17 +0200

    Adding first push of free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - free a list
+ * @head: pointer to the head
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+
+	int i = 0;
+	list_t *tmp = head;
+	list_t *tmp2 = tmp;
+
+	for (i = 0; tmp->next != NULL ; i++)
+	{
+		tmp2 = tmp->next;
+		free(tmp->str);
+		free(tmp);
+		tmp = tmp2;
+	}
+	free(tmp2->str);
+	free(tmp2);
+}
+
+
