4e61fbf453492c73b1a869f5f92c85412b9fb712
batrousseau <batrousseau@hotmail.com>
2025-07-13 16:35:00 +0200

    Fixing betty issues

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -9,20 +9,20 @@
  * Return: Nothing
  */
 
- void free_dlistint(dlistint_t * head)
+void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 	dlistint_t *tmp2;
 	int i = 0;
 
-	if(head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
 	tmp = head;
 	tmp2 = tmp;
 
-	for(i = 0; tmp->next !=NULL; i++)
+	for (i = 0; tmp->next != NULL; i++)
 	{
 		tmp2 = tmp->next;
 		free(tmp);
