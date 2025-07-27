c80efda898969cdc41eab5d215fd5ca58e0432b7
batrousseau <batrousseau@hotmail.com>
2025-07-27 14:57:52 +0200

    Trying something

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			/*If I'm right at the end of the list, it's ok*/
 			if (i == idx - 1)
 			{
-			return(add_dnodeint(h,n));
+			return(add_dnodeint_end(h,n));
 			}
 			/* If not, insertion is impossible */
 			return (NULL);
