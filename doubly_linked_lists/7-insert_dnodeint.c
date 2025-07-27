97e780b08e5593185e4296b88bb3f04539965e03
batrousseau <batrousseau@hotmail.com>
2025-07-27 14:09:07 +0200

    Preventing double insertion by exiting if my index brings me to the end of the list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -39,6 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (i == idx - 1)
 			{
 				add_dnodeint_end(h,n);
+				return (*h);
 			}
 			/* If not, insertion is impossible */
 			return (NULL);
