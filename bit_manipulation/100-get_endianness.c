d07ab0e3586cb7206132dae2154f9086931d2bd9
batrousseau <batrousseau@hotmail.com>
2025-08-12 12:17:54 +0200

    First try

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdint.h>
+
+/**
+ * get_endianess - check what type of endianess we are
+ * dealing with
+ * Return: 0 if big, 1 if little
+ */
+
+int get_endianness(void)
+{
+	int byte = 0;
+	int adress_byte_0 = (uintptr_t) &byte;
+	int adresse_byte_1 = (uintptr_t)&byte + sizeof(char);
+
+	if (adress_byte_0 < adresse_byte_1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
