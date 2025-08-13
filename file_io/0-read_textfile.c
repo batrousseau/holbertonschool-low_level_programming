b35b87c254fb6b56e92101c8346999cd86bdd627
batrousseau <batrousseau@hotmail.com>
2025-08-13 16:15:33 +0200

    betty changes and close insertion to file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,46 +6,50 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/**
+ * read_textfile - print a file through stdout
+ * @filename: name of the file
+ * @letters: number of chars to print
+ * Return: number of printed chars, -1 if failed
+ */
 
 size_t read_textfile(const char *filename, size_t letters)
 {
 	long int printed_char = 0;
 	int fd = 0;
 	int read_result = 0;
-	char *buff = NULL; 
+	char *buff = NULL;
+	int close_control = 0;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
-	buff = malloc((letters)*sizeof(char));
+	buff = malloc((letters) * sizeof(char));
 	if (buff == NULL)
 	{
 		return (-1);
 	}
-
 	read_result = read(fd, buff, letters);
 	if (read_result == -1)
 	{
 		return (-1);
 	}
-
-
 	printed_char = write(STDOUT_FILENO, buff, read_result);
 	if (printed_char == -1)
 	{
 		return (-1);
 	}
-
-	free (buff);
-
+	free(buff);
+	close_control = close(fd);
+	if (close_control == -1)
+	{
+		return (-1);
+	}
 	return (printed_char);
-
 }
