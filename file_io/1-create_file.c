1ff6034a290cffe6ca1a8af975f1701320bd4872
batrousseau <batrousseau@hotmail.com>
2025-08-13 17:23:45 +0200

    Fixing return for text is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -54,7 +54,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	txt_lght = get_lenght(text_content);
 	write_result = write(open_result, text_content, txt_lght);
