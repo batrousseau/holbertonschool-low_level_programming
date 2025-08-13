4b180a1430ab8df8a285c2acf4225e3dfc60697f
batrousseau <batrousseau@hotmail.com>
2025-08-13 17:07:46 +0200

    First push of task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * get_lenght - get the lenght of the text to insert
+ * @s: string to count
+ * Return: number of char to print
+ */
+
+int get_lenght(char *s)
+{
+	int i = 0;
+	int result = 0;
+
+	for (i = 0; *(s + i) != '\0' ; i++)
+	{
+	}
+	result = i;
+	return (result);
+}
+
+/**
+ * create_file - create a file
+ * @filename: name of the file to create
+ * @text_content: content of the text to insert into the newly
+ * created file
+ * Return: 1 ok, -1 failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int open_result = 0;
+	/*int read_result = 0;*/
+	int write_result = 0;
+	char *buff = NULL;
+	size_t txt_lght = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	open_result = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (open_result == -1)
+	{
+		return (-1);
+	}
+	printf("Open result is now %d \n", open_result);
+	if (text_content == NULL)
+	{
+		return (0);
+	}
+	txt_lght = get_lenght(text_content);
+	write_result = write(open_result, text_content, txt_lght);
+	if (write_result == -1)
+	{
+		printf("Write failed");
+		return (-1);
+	}
+	free(buff);
+	close(open_result);
+	return (1);
+
+}
--- file_io/main.h
+++ file_io/main.h
@@ -3,5 +3,7 @@
 #include <stdlib.h>
 
 size_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int get_lenght(char *s);
 
 #endif
